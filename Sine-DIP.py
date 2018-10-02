bb80a2f5232951130e41da82f9488db03ae69d6e
Sriram Ravula <sriram.ravula@utexas.edu>
2018-10-02 16:33:16 -0500

    Updated Sine-DIP for reusability

--- Sine-DIP.py
+++ Sine-DIP.py
@@ -23,7 +23,7 @@ import math
 
 LR = 1e-3 # learning rate
 MOM = 0.9 # momentum
-NUM_ITER = 100 # number iterations
+NUM_ITER = 80 # number iterations
 WD = 1e-4 # weight decay for l2-regularization
 
 Z_NUM = 32 # input seed
@@ -33,19 +33,19 @@ nc = 1 #num channels in the net I/0
 
 #choose the number of samples and periods in the training waveform
 WAVE_SIZE = 1024
-WAVE_PERIODS = 2
+WAVE_PERIODS = 1
 
 
 COMP = False
 
 if COMP:
-    num_measurements = 64
+    NUM_MEASUREMENTS = 64
 else:
-    num_measurements = WAVE_SIZE
+    NUM_MEASUREMENTS = WAVE_SIZE
 
 
 CUDA = torch.cuda.is_available()
-print(CUDA)
+print("Using CUDA: ", CUDA)
 
 #save the correct datatype depending on CPU or GPU execution
 if CUDA :
@@ -82,48 +82,53 @@ def normalise(x, mu, sigma):
 def renormalise(x, mu, sigma):
     return x*sigma + mu
 
-
-mse = torch.nn.MSELoss().type(dtype)
-
 #num_harmonics selects how many superimposed waveforms to train on, gan selects which net topology to use
-def run(num_harmonics = 1, gan = 2):
+def run(num_harmonics = 1, gan = 2, num_iter = 80, wave_size = 1024, wave_periods = 2, num_measurements = 1024, cuda = True, compressed = False, plot_waves = False, filters_per_layer = 64, batch_size = 1, num_channels = 1, seed_size = 32):
 
-    mse_log = np.zeros((NUM_ITER)) #track the MSE between the denoised and net output
-    best_wave = np.zeros((WAVE_SIZE)) #track the best waveform
+    mse_log = np.zeros((num_iter)) #track the MSE between the denoised and net output
+    best_wave = np.zeros((wave_size)) #track the best waveform
     cur_best_mse = 1e6 #track the current best MSE
 
     if(gan==0):
-        net = nets.DCGAN_Straight(Z_NUM, NGF, WAVE_SIZE, nc, num_measurements, cuda = CUDA)
+        net = nets.DCGAN_Straight(seed_size, filters_per_layer, wave_size, num_channels, num_measurements, cuda = cuda)
     elif(gan==1):
-        net = nets.DCGAN_Funnel(Z_NUM, NGF, WAVE_SIZE, nc, num_measurements, cuda = CUDA)
+        net = nets.DCGAN_Funnel(seed_size, filters_per_layer, wave_size, num_channels, num_measurements, cuda = cuda)
     elif(gan==2):
-        net = nets.DCGAN_Straight_Exhaustive(Z_NUM, NGF, WAVE_SIZE, nc, num_measurements, cuda = CUDA)
+        net = nets.DCGAN_Straight_Exhaustive(seed_size, filters_per_layer, wave_size, num_channels, num_measurements, cuda = cuda)
     elif(gan==3):
-        net = nets.DCGAN_Funnel_Exhaustive(Z_NUM, NGF, WAVE_SIZE, nc, num_measurements, cuda = CUDA)
+        net = nets.DCGAN_Funnel_Exhaustive(seed_size, filters_per_layer, wave_size, num_channels, num_measurements, cuda = cuda)
     else:
         quit()
 
     net.fc.requires_grad = False
 
-    if CUDA:  # move network to GPU if available
+    if cuda:  # move network to GPU if available
         net.cuda()
 
-    if COMP:
-        net.fc.weight.data = (1 / math.sqrt(1.0 * num_measurements)) * torch.randn(num_measurements, WAVE_SIZE * nc)
+    if compressed:
+        net.fc.weight.data = (1 / math.sqrt(1.0 * num_measurements)) * torch.randn(num_measurements, wave_size * num_channels)
     else:
-        net.fc.weight.data = torch.eye(num_measurements)
+        net.fc.weight.data = torch.eye(wave_size)
 
     allparams = [x for x in net.parameters()]  # specifies which to compute gradients of
     allparams = allparams[:-1]  # get rid of last item in list (fc layer) because it's memory intensive
 
-    z = Variable(torch.zeros(ALEX_BATCH_SIZE * Z_NUM).type(dtype).view(ALEX_BATCH_SIZE, Z_NUM, 1))
+    z = Variable(torch.zeros(batch_size * seed_size).type(dtype).view(batch_size, seed_size, 1))
     z.data.normal_().type(dtype)
 
     # Define optimizer
     optim = torch.optim.RMSprop(allparams, lr=LR, momentum=MOM, weight_decay=WD)
 
-    y0 = get_sinusoid(num_samples=WAVE_SIZE, num_periods=WAVE_PERIODS, num_harmonics = num_harmonics, noisy=True)
-    y0_denoised = get_sinusoid(num_samples=WAVE_SIZE, num_periods=WAVE_PERIODS, num_harmonics=num_harmonics, noisy=False)
+    y0 = get_sinusoid(num_samples=wave_size, num_periods=wave_periods, num_harmonics = num_harmonics, noisy=True)
+    y0_denoised = get_sinusoid(num_samples=wave_size, num_periods=wave_periods, num_harmonics=num_harmonics, noisy=False)
+
+    # Plot both noisy (blue) and denoised (red) waveforms
+    if plot_waves:
+        plt.plot(np.arange(1024), y0)
+        plt.plot(np.arange(1024), y0_denoised, color='r')
+        plt.xlabel("Sample")
+        plt.ylabel("Value")
+        plt.show()
 
     MU = get_stats(y0)[0]
     SIGMA = get_stats(y0)[1]
@@ -132,12 +137,14 @@ def run(num_harmonics = 1, gan = 2):
     y = normalise(y, MU, SIGMA)
     y = Variable(y.type(dtype))
 
-    measurements = Variable(torch.mm(y.cpu().data.view(ALEX_BATCH_SIZE, -1), net.fc.weight.data.permute(1, 0)), requires_grad=False)
+    measurements = Variable(torch.mm(y.cpu().data.view(batch_size, -1), net.fc.weight.data.permute(1, 0)), requires_grad=False)
 
-    if CUDA:  # move measurements to GPU if possible
+    if cuda:  # move measurements to GPU if possible
         measurements = measurements.cuda()
 
-    for i in range(NUM_ITER):
+    mse = torch.nn.MSELoss().type(dtype)
+
+    for i in range(num_iter):
         optim.zero_grad()  # clears graidents of all optimized variables
         out = net(z)  # produces wave (in form of data tensor) i.e. G(z,w)
 
@@ -159,4 +166,10 @@ def run(num_harmonics = 1, gan = 2):
 
     return [mse_log, best_wave]
 
-print(np.argmin(run()[0]))
+best_log = np.zeros((50))
+for i in range(50):
+    best_log[i] = np.argmin(run()[0])
+
+print("Best Iteration: ", best_log)
+print("Mean Best Iteration: ", np.mean(best_log))
+#print("best iteration: ", np.argmin(run()[0]))
