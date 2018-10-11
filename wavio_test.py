2fa039c2bd9e5e93958711d47a82acd3eec8e019
Sriram Ravula <sriram.ravula@utexas.edu>
2018-10-11 16:15:11 -0500

    Fleshed out audio neural net, initial tests

--- wavio_test.py
+++ wavio_test.py
@@ -3,7 +3,7 @@ import wavio
 import matplotlib.pyplot as plt
 
 rate = 8192             # samples per second
-T = 4                    # sample duration (seconds)
+T = 2                    # sample duration (seconds)
 f = 500.0                # sound frequency (Hz)
 t = np.linspace(0, T, T*rate, endpoint=False)
 sig = np.sin(2 * np.pi * f * t)
@@ -17,7 +17,7 @@ print(np.min(wav.data[:, 0]))
 
 wav = wavio.read("sine24.wav")
 print("rate: ", wav.rate)
-print("length (s): ", round(wav.data.shape[0]/(1.0*wav.rate), 4))
+print("length: ", wav.data.shape[0])
 print("bits/sample: ", 8*wav.sampwidth)
 print(np.max(wav.data[:, 0]))
 
