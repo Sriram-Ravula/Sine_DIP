b3cd38b8bc4687e58990a8144252f3131ddaee28
Sriram Ravula <sriram.ravula@utexas.edu>
2018-10-12 16:33:57 -0500

    Completed audio testing, added visualisations

--- wavio_test.py
+++ wavio_test.py
@@ -4,16 +4,10 @@ import matplotlib.pyplot as plt
 
 rate = 8192             # samples per second
 T = 2                    # sample duration (seconds)
-f = 500.0                # sound frequency (Hz)
+f = 350.0                # sound frequency (Hz)
 t = np.linspace(0, T, T*rate, endpoint=False)
 sig = np.sin(2 * np.pi * f * t)
 sig = sig + np.sin(2 * np.pi * 4*f * t)
-
-wav = wavio.read("lowres.wav")
-print("rate: ", wav.rate)
-print("length (s): ", round(wav.data.shape[0]/(1.0*wav.rate), 4))
-print("bits/sample: ", 8*wav.sampwidth)
-print(np.min(wav.data[:, 0]))
 
 wav = wavio.read("sine24.wav")
 print("rate: ", wav.rate)
@@ -21,4 +15,8 @@ print("length: ", wav.data.shape[0])
 print("bits/sample: ", 8*wav.sampwidth)
 print(np.max(wav.data[:, 0]))
 
-wavio.write("sine24.wav", sig, rate, sampwidth=2)
+wave = np.zeros((len(sig), 2))
+wave[:, 0] = sig
+wave[:, 1] = np.sin(2 * np.pi * 8*f * t)
+
+wavio.write("sine24.wav", wave, rate, sampwidth=2)
