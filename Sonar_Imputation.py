d691e17a8ffbf7bb38b0f09d063bbe825ede4121
Sriram Ravula <sriram.ravula@utexas.edu>
2019-07-12 15:01:58 -0500

    Changing normalisation

--- Sonar_Imputation.py
+++ Sonar_Imputation.py
@@ -12,7 +12,7 @@ import pywt
 #NETWORK SETUP
 LR = 1e-4 # learning rate
 MOM = 0.9 # momentum
-NUM_ITER = 3000 # number iterations
+NUM_ITER = 2000 # number iterations
 WD = 1 # weight decay for l2-regularization
 Z_NUM = 32 # input seed
 NGF = 64 # number of filters per layer
@@ -55,13 +55,12 @@ plt.show()
 
 
 #IMPUTATION SETUP
-kept_samples = [x for x in range(LENGTH) if x not in range(405, 425)]
+kept_samples = [x for x in range(LENGTH) if x not in range(390, 420)]
 A = np.identity(LENGTH)[kept_samples, :] #the dropout transformation
 
 y = x[kept_samples]
 
 x_hat = dip_utils.run_DIP_short(A, y, dtype, NGF = NGF, LR=LR, MOM=MOM, WD=WD, output_size=LENGTH, num_measurements=len(kept_samples), CUDA=CUDA, num_iter=NUM_ITER, alpha_tv=alpha_tv)
-
 
 plt.figure()
 plt.plot(range(LENGTH), x_hat, label = "Imputed")
@@ -73,10 +72,8 @@ plt.show()
 orig_normalised = s.two_pass_filtering(x, 20, target_len+5, 1)
 imputed_normalised = s.two_pass_filtering(x_hat, 20, target_len+5, 1)
 
-"""
 plt.figure()
 plt.plot(range(LENGTH), imputed_normalised, label = "Imputed")
 plt.plot(range(LENGTH), orig_normalised, label = "Original")
 plt.legend()
 plt.show()
-"""
