137ae920149c98ceb0cea6053da6d8cecfa0e379
Sriram Ravula <sriram.ravula@utexas.edu>
2019-07-17 15:12:42 -0500

    updated normalization parameters

--- Sonar_Imputation.py
+++ Sonar_Imputation.py
@@ -14,7 +14,7 @@ from scipy.interpolate import interp1d
 #NETWORK SETUP
 LR = 1e-4 # learning rate
 MOM = 0.9 # momentum
-NUM_ITER = 2000 # number iterations
+NUM_ITER = 3000 # number iterations
 WD = 1 # weight decay for l2-regularization
 Z_NUM = 32 # input seed
 NGF = 64 # number of filters per layer
@@ -38,6 +38,7 @@ else:
 signal = s.gen_data(LENGTH, 0.5, 1)
 
 target_range = np.array(range(400, 430)) #the range in the signal where the target appears
+target_range = np.union1d(target_range, np.array(range(800, 830)))
 target_len = len(list(target_range))
 
 signal[target_range] += 0.04 #add artificial target to the background signal
@@ -57,59 +58,64 @@ plt.show()
 
 
 #IMPUTATION SETUP
-missing_samples = range(390, 420)
+missing_samples = np.array(range(395, 425))
+missing_samples = np.union1d(missing_samples, np.array(range(805, 835)))
+missing_samples = np.union1d(missing_samples, np.array(range(200, 230)))
+
 kept_samples = [x for x in range(LENGTH) if x not in missing_samples]
+
 A = np.identity(LENGTH)[kept_samples, :] #the dropout transformation
 
 y = x[kept_samples]
 
+#DIP imputation
 #x_hat = dip_utils.run_DIP_short(A, y, dtype, NGF = NGF, LR=LR, MOM=MOM, WD=WD, output_size=LENGTH, num_measurements=len(kept_samples), CUDA=CUDA, num_iter=NUM_ITER, alpha_tv=alpha_tv)
 
-#A = spfft.idct(np.identity(LENGTH), norm='ortho', axis=0)[kept_samples, :]
-#x_hat_lasso = inverse_utils.run_Lasso(A=A, y=y, output_size=LENGTH, alpha=1e-5)
+#Lasso imputation
+phi = spfft.idct(np.identity(LENGTH), norm='ortho', axis=0)[kept_samples, :]
+x_hat_lasso = inverse_utils.run_Lasso(A=phi, y=y, output_size=LENGTH, alpha=1e-5)
 
+#Signal with missing values
 x_missing = x.copy()
 x_missing[missing_samples] = None
 
+#Gaussian imputation
 x_hat_gaussian = x.copy()
-x_hat_gaussian[missing_samples] = np.random.normal(np.mean(y),np.std(y),(len(missing_samples), 1))
+x_hat_gaussian[missing_samples] = np.random.normal(np.mean(y), 0.1, (len(missing_samples), 1))
 
+#Linear interpolation
 f_interp = interp1d(x = kept_samples, y = x[kept_samples].squeeze() + 2, kind='linear')
 interped = (f_interp(missing_samples) - 2)
 x_hat_interp = x.copy()
 x_hat_interp[missing_samples] = interped.reshape(-1, 1)
 
+#Plotting signals before imputation
 plt.figure()
-#plt.plot(range(LENGTH), x_hat, label = "Imputed")
-#plt.plot(range(LENGTH), x_hat_lasso, label = "Imputed")
-#plt.plot(range(LENGTH), x, label = "Original")
-#plt.plot(range(LENGTH), x_hat_gaussian, label = "Gaussian-filled")
-plt.plot(range(LENGTH), x_hat_interp, label = "Linear Interpolation")
+#plt.plot(range(LENGTH), x_hat, label = "DIP Imputation", color = 'b')
+plt.plot(range(LENGTH), x_hat_lasso, label = "Imputed", color = 'g')
+#plt.plot(range(LENGTH), x, label = "Original", color = 'r')
+#plt.plot(range(LENGTH), x_hat_gaussian, label = "Gaussian-filled", color = 'c')
+#plt.plot(range(LENGTH), x_hat_interp, label = "Linear Interpolation", color = 'm')
+#plt.plot(range(LENGTH), x_missing, label = "With Missing Values", color = 'k')
 plt.legend()
 plt.grid(True)
+plt.title("Deep Image Prior")
 plt.show()
 
-
-"""
-plt.figure()
-plt.plot(range(LENGTH), x_missing, label = "W/Missing Values", color = 'green')
-plt.legend()
-plt.grid(True)
-plt.show()
-"""
-
-#orig_normalised = s.two_pass_filtering(x, 20, target_len+5, 1.5)
-#imputed_normalised = s.two_pass_filtering(x_hat, 20, target_len+5, 1.5)
-#lasso_normalised = s.two_pass_filtering(x_hat_lasso, 20, target_len+5, 1.5)
-#gaussian_normalised = s.two_pass_filtering(x_hat_gaussian, 20, target_len+5, 1.5)
-interp_normalised = s.two_pass_filtering(x_hat_interp, 20, target_len+5, 1.5)
+#Normalize imputed signals
+#orig_normalised = s.two_pass_filtering(x+2, 20, 35, 1)
+#imputed_normalised = s.two_pass_filtering(x_hat+2, 20, 35, 1)
+lasso_normalised = s.two_pass_filtering(x_hat_lasso + 2, 20, 35, 1)
+#gaussian_normalised = s.two_pass_filtering(x_hat_gaussian + 2, 20, 35, 1)
+#interp_normalised = s.two_pass_filtering(x_hat_interp + 2, 20, 35, 1)
 
 plt.figure()
-#plt.plot(range(LENGTH), imputed_normalised, label = "Imputed")
-#plt.plot(range(LENGTH), lasso_normalised, label = "LASSO", color='r')
-#plt.plot(range(LENGTH), orig_normalised, label = "Original")
-#plt.plot(range(LENGTH), gaussian_normalised, label = "Gaussian-filled", color = 'k')
-plt.plot(range(LENGTH), interp_normalised, label = "Linear Interpolation", color = 'm')
+#plt.plot(range(LENGTH), imputed_normalised, label = "DIP Imputation", color = 'b')
+plt.plot(range(LENGTH), lasso_normalised, label = "Lasso", color = 'g')
+#plt.plot(range(LENGTH), orig_normalised, label = "Original", color = 'r')
+#plt.plot(range(LENGTH), gaussian_normalised, label = "Gaussian-filled", color = 'c')
+#plt.plot(range(LENGTH), interp_normalised, label = "Linear Interpolation", color = 'm')
+plt.ylim(0.5, 1.5)
 plt.legend()
 plt.grid(True)
 plt.show()
